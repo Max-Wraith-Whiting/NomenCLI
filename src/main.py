205993a79b38b38cb6253721ee2d5b12ffabb3ac
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-06-20 19:13:46 +0100

    Commented out 'name' command. To be modified... 'name' should be modified into a scrambler tool.

--- src/main.py
+++ src/main.py
@@ -14,39 +14,39 @@ app.add_typer(person.app, name="person")
 app.add_typer(place.app, name="place")
 app.add_typer(thing.app, name="thing")
 
-@app.command()
-def name():
-    word = input("Enter a word that relates to that which intend to name. \n> ")
-    permutations = itertools.permutations([*word])
-    permutations = ["".join(word) for word in permutations]
-    print("Enter the number of one the following words you like.")
+# @app.command()
+# def name():
+#     word = input("Enter a word that relates to that which intend to name. \n> ")
+#     permutations = itertools.permutations([*word])
+#     permutations = ["".join(word) for word in permutations]
+#     print("Enter the number of one the following words you like.")
     
-    # Select 20 words or less word
-    words_to_select = 20 if len(permutations) > 20 else len(permutations)
-    selection = []
-    while words_to_select != 0:
-        random_word = random.choice(permutations)
-        selection.append(random_word)
-        permutations.remove(random_word)
-        words_to_select -= 1
+#     # Select 20 words or less word
+#     words_to_select = 20 if len(permutations) > 20 else len(permutations)
+#     selection = []
+#     while words_to_select != 0:
+#         random_word = random.choice(permutations)
+#         selection.append(random_word)
+#         permutations.remove(random_word)
+#         words_to_select -= 1
     
-    count = 1
-    for word in selection:
-        print(f"\t {count}. {word}")
-        count += 1
+#     count = 1
+#     for word in selection:
+#         print(f"\t {count}. {word}")
+#         count += 1
         
-    print("Enter 0 for to generate more names:")
+#     print("Enter 0 for to generate more names:")
     
-    chosen_word = None
-    while chosen_word not in range(1,count):
-        try:
-            chosen_word = int(input("> "))
-        except:
-            print("Please enter a valid number.")
+#     chosen_word = None
+#     while chosen_word not in range(1,count):
+#         try:
+#             chosen_word = int(input("> "))
+#         except:
+#             print("Please enter a valid number.")
             
-    print("\n Now that you have your word, try and say it. \n If you have trouble try adding some vowels around the troublesome parts.")
-    word = selection[chosen_word - 1]
-    print(f" {word} is an interesting choice to say the least.")
+#     print("\n Now that you have your word, try and say it. \n If you have trouble try adding some vowels around the troublesome parts.")
+#     word = selection[chosen_word - 1]
+#     print(f" {word} is an interesting choice to say the least.")
 
 @app.command()
 def place():
