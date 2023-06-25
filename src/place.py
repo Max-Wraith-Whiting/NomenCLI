5c9bf8e12480b368920c069f8291d403365c5727
Nexus <Max.Wraith-Whiting@protonmail.com>
2023-06-25 19:52:35 +0100

    New generator idea

--- src/place.py
+++ src/place.py
@@ -6,8 +6,11 @@ APP_NAME = "NomenCLI"
 app = typer.Typer()
 
 @app.command()
-def stub():
-    print("place stub was called!")
+def landform(amount_of_names: Annotated[Optional[int], typer.Argument(callback=amount_of_names_callback)] = 10,
+          save: Annotated[bool, typer.Option("--save", "-s", help="Save the generated names to the configured save location.")] = False,
+          filename: Annotated[str, typer.Option(help="The specified filename if the --save/-s option is chosen.")] = "Names"):
+    pass
+    
 
 if __name__ == "__main__":
     app()
